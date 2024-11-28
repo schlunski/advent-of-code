5da5c9eb0d7fe9efa2a646697bcdb16eaa6fca05
schlunski <sheinrich2002@gmail.com>
2024-11-28 21:11:42 +0100

    Updated documentation and default value

--- include/reader.h
+++ include/reader.h
@@ -8,7 +8,7 @@
  * read a Line from the file
  *
  * @param[in] file file to read a line from
- * @param[in] delimiter by default no delimiter
+ * @param[in] delimiter seperator for the lines, by default no delimiter
  *
  * @return words in line seperated by delimiter
  */
@@ -18,14 +18,14 @@ std::vector<std::string> readLine(std::fstream& file, char delimiter = '\0');
  * read the full file:
  *
  * @param[in] file file to read a line from
- * @param[in] delimiter seperator for the lines
+ * @param[in] delimiter seperator for the lines, by default no delimiter
  * @param[in] skipLines deticates how many line in the file should be skipped
  * @param[in] collumwise deticates if sorted collum- or rowwise
  *
  * @return Table of word in the file
  */
 std::vector<std::vector<std::string>> readFullFile(
-    std::fstream& file, char delimiter, int skipLines = 0,
+    std::fstream& file, char delimiter = '\0', int skipLines = 0,
     bool columnwise = true);
 
 /**
